538652f87edb1564350624eba55d3434baaaf6aa
eng-OmarAdel <33545345+eng-OmarAdel@users.noreply.github.com>
2019-05-14 16:21:05 +0200

    adjusting baseCase to include -sloped skew

--- ai.py
+++ ai.py
@@ -1,8 +1,8 @@
 currentState=[['0' for col in range(7)] for row in range(6)]
-currentState[5][0]='R'
-currentState[4][0]='R'
-currentState[3][0]='R'
-currentState[2][0]='R'
+currentState[5][1]='R'
+currentState[4][2]='R'
+currentState[3][3]='R'
+currentState[2][4]='R'
 def isComplete(row,start,end):
     for i in range(start+1,end):
         if row[i]!=row[i-1]:
@@ -14,18 +14,22 @@ def isBaseCase(state):
             if state[row][col] == '0':
                 continue
             if col+4 <= 7:
-                result = isComplete(state[row],col,col+4)
+                vec=[state[row][col],state[row][col+1],state[row][col+2],state[row][col+3]]
+                result = isComplete(vec,0,4)
                 if result != 0:
                     return result
             if row+4 <= 6:
-                print(state[row][col])
                 vec=[state[row][col],state[row+1][col],state[row+2][col],state[row+3][col]]
-                print(vec)
                 result = isComplete(vec,0,4)
                 if result != 0:
                     return result
             if (col+4 <= 7) and (row+4 <= 6):
                 vec = [state[col][row],state[col+1][row+1],state[col+2][row+2],state[col+3][row+3]]
+                result = isComplete(vec,0,4)
+                if result != 0:
+                    return result
+            if (col+4 <= 7) and (row-4 >= 0):
+                vec = [state[col][row],state[col+1][row-1],state[col+2][row-2],state[col+3][row-3]]
                 result = isComplete(vec,0,4)
                 if result != 0:
                     return result
