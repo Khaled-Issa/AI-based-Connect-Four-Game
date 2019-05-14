edf25c3039aadbef6231e9334aabe182fc963f66
Omar M. Adel <eng.omaradel95@yahoo.com>
2019-05-14 17:53:02 +0200

    Update ai.py

--- ai.py
+++ ai.py
@@ -30,7 +30,7 @@ def isBaseCase(state):
                 result = isComplete(vec,0,4)
                 if result != 0:
                     return result
-            if (col-4 >= 0) and (row+4 <= 6):
+            if (col-3 >= 0) and (row+4 <= 6):
                 print(row,col)
                 vec = [state[row][col],state[row+1][col-1],state[row+2][col-2],state[row+3][col-3]]
                 result = isComplete(vec,0,4)
