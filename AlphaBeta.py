5dd8d147121050469112205ca9d93810bab7573e
eng-OmarAdel <33545345+eng-OmarAdel@users.noreply.github.com>
2019-05-15 22:38:48 +0200

    Now we need to test if the game is working properly, i hope it would :D

--- AlphaBeta.py
+++ AlphaBeta.py
@@ -9,104 +9,108 @@ INF = 100000
 NEGINF = -1
 # return all the state successors
 def Successors(state, turn):
-	print("I'm here")
-	successors = []
-	tempState = deepcopy(state)
-	for col in range(7):
-		for row in reversed(range(6)):
-			if state[row][col] == '0':
-				tempState = deepcopy(state)
-				tempState [row][col] = turn
-				successors.append(tempState)
-				break
-	###A list containing the direct children of the state as the first element
-	###And the move 
-	successor_Path=[successors,moves]
-	return successor_Path
+        print("I'm here")
+        successors = []
+        moves=[]
+        tempState = deepcopy(state)
+        for col in range(7):
+                for row in reversed(range(6)):
+                        if state[row][col] == '0':
+                                tempState = deepcopy(state)
+                                tempState [row][col] = turn
+                                successors.append(tempState)
+                                moves.append(col)
+                                break
+        ###A list containing the direct children of the state as the first element
+        ###And the move 
+        successor_Path=[successors,moves]
+        return successor_Path
 # return the minmum value of this state
 def MinValue(state, Alpha, Beta,path):
-	# Check if the current state is a terminal one "win/draw"
-	# if yes return the utitilty "R/G/0"
-	utitilty = isBaseCase(state)
-	if utitilty != "0":
-		return utitilty
-	
-	value = INF
+        # Check if the current state is a terminal one "win/draw"
+        # if yes return the utitilty "R/G/0"
+        utitilty = isBaseCase(state)
+        if utitilty != "0":
+                return utitilty
+        
+        value = INF
 
-	# loop in all successors/children
-	for x in Successors(state,'G')[0]:
-                path.append(Successors(state,'G')[1])
-		maxValue = ord(MaxValue(x, Alpha, Beta, path))
-		value = min(value, maxValue)
-		if value <= Alpha:
-			return value
-		###I dont' know what is that doing
-		Beta = max(Beta, value)
-	return value
+        # loop in all successors/children
+        successors_path=Successors(state,'G')
+        for childState, move in zip(successors_path[0],successors_path[1]):
+                path.append(move)
+                maxValue = ord(MaxValue(childState, Alpha, Beta, path))
+                value = min(value, maxValue)
+                if value <= Alpha:
+                        return value
+                ###I dont' know what is that doing
+                Beta = max(Beta, value)
+        return value
 
 # return the max value of this state
 def MaxValue(state, Alpha, Beta,path):
-	# Check if the current state is a terminal one "win/draw"
-	# if yes return the utitilty "R/G/0"
-	utitilty = isBaseCase(state)
-	if utitilty != "0":
-		return utitilty
-	
-	value = NEGINF
+        # Check if the current state is a terminal one "win/draw"
+        # if yes return the utitilty "R/G/0"
+        utitilty = isBaseCase(state)
+        if utitilty != "0":
+                return utitilty
+        
+        value = NEGINF
 
-	# loop in all successors/children
-	for x in Successors(state,'R')[0]:
-                path.append(Successors(state,'G')[1])
-		minValue = ord(MinValue(X,Alpha,Beta, path))
-		value = max(value, minValue)
-		if value >= Beta:
-			return value
-		###I dont' know what is that doing
-		Alpha = max(Alpha, value)
-	return value
+        # loop in all successors/children
+        successors_path=Successors(state,'R')
+        for childState, move in zip(successors_path[0],successors_path[1]):
+                path.append(move)
+                minValue = ord(MinValue(childState,Alpha,Beta, path))
+                value = max(value, minValue)
+                if value >= Beta:
+                        return value
+                ###I dont' know what is that doing
+                Alpha = max(Alpha, value)
+        return value
 
 # return action to the Interface function >> NOT DONE YET
 def AlphaBeta():
-	# Playing Logic
-	board = make_board()
-	pboard=np.array(board)
+        # Playing Logic
+        board = make_board()
+        pboard=np.array(board)
 
-	no_winner = True
-	turn = 0 
+        no_winner = True
+        turn = 0 
 
-	pretty_print(pboard)
-	while no_winner:
-	    #player 1
-	    if turn == 0:
-	        col = int(input("choose where to play (from 0 to 6):"))
+        pretty_print(pboard)
+        while no_winner:
+            #player 1
+            if turn == 0:
+                col = int(input("choose where to play (from 0 to 6):"))
 
-	        while col > 6 or col < 0:
-	            col = int(input("choose where to play (from 0 to 6):"))
+                while col > 6 or col < 0:
+                    col = int(input("choose where to play (from 0 to 6):"))
 
-	        if column_is_not_full(board, col):
-	            row= open_row_position(board, col)
-	            drop_piece(board, row, col, "G")
+                if column_is_not_full(board, col):
+                    row= open_row_position(board, col)
+                    drop_piece(board, row, col, "G")
    
-	    else:
-	        # player 2 >> CHANGE THE 2ND PLAYER TO BOARD
-	        # Tree, Alpha Beta pruning
-	        path=[]
-	    	v = MaxValue(board, NEGINF,INF ,path)
-	    	# IS V the brnahes value ? Nope
-	    	# CREATE ACTION HERE
-	    	if column_is_not_full(board, col):
-	        	row=open_row_position(board, col)
-	        	drop_piece(board,row,col,"R")
-	    
-	    # Flip board
-	    fboard = np.flip(board,0)
-	    print(fboard)
-	    
-	    # switch turn       
-	    turn = turn + 1
-	    turn = turn % 2
+            else:
+                # player 2 >> CHANGE THE 2ND PLAYER TO BOARD
+                # Tree, Alpha Beta pruning
+                path=[]
+                v = MaxValue(board, NEGINF,INF ,path)
+                # IS V the brnahes value ? Nope
+                # CREATE ACTION HERE
+                if column_is_not_full(board, col):
+                        row=open_row_position(board, col)
+                        drop_piece(board,row,col,"R")
+            
+            # Flip board
+            fboard = np.flip(board,0)
+            print(fboard)
+            
+            # switch turn       
+            turn = turn + 1
+            turn = turn % 2
 
-	  
+          
 
 
 AlphaBeta()
